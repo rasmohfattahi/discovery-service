fa7d1d1516b9a63061c2a33caf2a3bc0b5f099bf
rasool.fattahi <nahalfattahi2010@gmail.com>
2020-10-15 20:17:00 +0200

    update version

--- src/main/java/com/mft/cloud/discovery/DiscoveryServer.java
+++ src/main/java/com/mft/cloud/discovery/DiscoveryServer.java
@@ -1,4 +1,4 @@
-package com.fattahi.cloud.discovery;
+package com.mft.cloud.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
